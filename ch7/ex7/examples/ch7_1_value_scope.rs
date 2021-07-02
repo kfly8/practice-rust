a02a794bf4dc4ed0ccf74dfdd3bc39a94dd7f2d6
kfly8 <kfly@cpan.org>
2021-07-02 23:49:35 +0900

    ch7/ex7/value_scope

---
+++ ch7/ex7/examples/ch7_1_value_scope.rs
@@ -0,0 +1,32 @@
+#[derive(Debug)]
+struct Parent(usize, Child, Child);
+
+#[derive(Debug)]
+struct Child(usize);
+
+use std::ops::Drop;
+
+impl Drop for Parent {
+    fn drop(&mut self) {
+        println!("Dropping {:?}", self);
+    }
+}
+
+impl Drop for Child {
+    fn drop(&mut self) {
+        println!("Dropping {:?}", self);
+    }
+}
+
+fn main() {
+    let p1 = Parent(1, Child(11), Child(12));
+
+    {
+        let p2 = Parent(2, Child(21), Child(22));
+        println!("(a) p1: {:?}, p2: {:?}", p1, p2);
+    }
+
+    println!("(b) p1: {:?}", p1);
+    let p3 = Parent(3, Child(31), Child(32));
+    println!("(c) p1: {:?}, p3: {:?}", p1, p3);
+}
